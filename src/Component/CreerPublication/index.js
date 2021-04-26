39a4da2ab7c2a59dbccfd4062b2e42a9071f0a92
Saber Mrabet <mrabetsaber31@gmail.com>
2021-04-26 10:09:42 +0100

    creer une publication

--- src/Component/CreerPublication/index.js
+++ src/Component/CreerPublication/index.js
@@ -6,6 +6,8 @@ import { Collapse, IconButton, makeStyles } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import clsx from 'clsx';
+import CloseIcon from '@material-ui/icons/Close';
 const useStyles = makeStyles((theme) => ({
 
 
@@ -18,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
         marginTop:40,
         marginBottom:20,
         flexGrow:1,
+        
       },
       root1:{
           display:'flex'
@@ -39,60 +42,80 @@ const useStyles = makeStyles((theme) => ({
           marginLeft:theme.spacing(10)
       },
       publier:{
-        maxHeight:800,
-        maxWidth: 800,
-        marginLeft:'25%',
-       textAlign:'center',
-       marginTop:'-5%',
-       backgroundColor:"rgba(117, 190, 218, 0.0)",
+        height:300,
+        maxHeight:600,
+        
+        width:640,
+        marginLeft:'30%',
+       alignItems:'center',
+       marginTop:40,
+      
        
-      }
+      },
+      rootClicked:{
+        visibility:'hidden',
+      },
+      header:{
+        
+       display:'flex',
+       flexGrow:1,
+        fontWeight:900,
+        alignItems:'center',
+        padding: '10px 10px',
+        width: 640,
+        marginLeft:'30%',
+        marginTop:40,
+        marginBottom:20,
+        flexGrow:1,
+      },
+      closeIco:{
+          marginLeft:'15%'
+      },
+      PublicationInput:{
+          paddingLeft:'100px',
+          height:100,
+          width:'100%',
+         textAlign:'center',
+         maxHeight:200,
+         marginBottom:10
+      },
+      avatarP:{
+        backgroundColor: red[500],
+        marginLeft:theme.spacing(6)
+      },
     }));
 function Index() {
     const classes=useStyles();
-    const [commentaire,setCommentaire]=useState('')
-    const comment = (e)=>{
-        
-       
+    const [visabal,setvisabal]=useState(true);
+    const handleVisbalClick = ()=>{
+      
+        setvisabal(!visabal);
+      
     }
     return (
         <div>
-            <Paper component="form"  className={classes.root} >
-                <div className={classes.root1}>
-                    <Avatar aria-label="recipe" className={classes.avatar}>
-                            R
-                    </Avatar>
-    
-                    <InputBase
-                        className={classes.input}
-                        placeholder="Comment..."
-                        onClick={comment}
-                        
-                    />
+          
+            <Collapse  in={visabal}>
+            <Paper  className={classes.publier}  elevation={3}>
+
+                <div className={classes.header}>
+                    <h3>Creer un publication </h3>
+                <IconButton className={classes.closeIco} onClick={handleVisbalClick}>
+                <CloseIcon/>
+                </IconButton>
                 </div>
                 <div >
-                <IconButton size="small" className={classes.icon} >
-                    <PhotoLibraryIcon color="primary"/>
-                    Picture
-                </IconButton>
-                <IconButton size="small" className={classes.icon1}>
-                    <YouTubeIcon color="secondary"/>
-                    Video
-                </IconButton>
-                </div>
-            </Paper>
-            <Collapse in="true">
-            <Paper className={classes.publier}  elevation={3}>
-                <h3>Creer un publication </h3>
-                <Avatar aria-label="recipe" className={classes.avatar}>
+                <Avatar aria-label="recipe" className={classes.avatarP}>
                             R
                     </Avatar>
                     <InputBase
-                        className={classes.input}
+                        className={classes.PublicationInput}
                         placeholder="Que Voulez-vous dire "
-                       
+                        multiline
+                        rowsMax={6}
                         
                     />
+                    </div>
                     <IconButton size="small" className={classes.icon} >
                     <PhotoLibraryIcon color="primary"/>
                     Picture
@@ -101,8 +124,35 @@ function Index() {
                     <YouTubeIcon color="secondary"/>
                     Video
                 </IconButton>
+
             </Paper>
             </Collapse>
+            <Paper component="form"   className={clsx(classes.root, {
+            [classes.rootClicked]: visabal,})} >
+
+                <div className={classes.root1}>
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                            R
+                    </Avatar>
+    
+                    <InputBase
+                        className={classes.input}
+                        placeholder="Comment..."
+                        onClick={handleVisbalClick}
+                        
+                    />
+                </div>
+                
+                <IconButton size="small" className={classes.icon} >
+                    <PhotoLibraryIcon color="primary"/>
+                    Picture
+                </IconButton>
+                <IconButton size="small" className={classes.icon1}>
+                    <YouTubeIcon color="secondary"/>
+                    Video
+                </IconButton>
+                
+            </Paper>
         </div>
     )
 }
--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -3,13 +3,14 @@ import Navbar from '../../Component/Navbar';
 import Publication from '../../Component/Publication';
 import CreatePublication from '../../Component/CreerPublication';
 import './style.css'
+
 function index() {
   
     return (
         <div className="root" >
             <Navbar/>
             <CreatePublication/>
-
+           
 
             <Publication/>
         </div>
